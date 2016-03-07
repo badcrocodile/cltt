c443114aa67807594d38b586b90b4c9b221912bb
Jason Pollock <jason@monkeyhousemarketing.com>
2016-03-07 11:51:32 -0500

    Extracted a lot of procedural code into static methods. Going to try and create an instance of time as a new class.

--- src/FormatTime.php
+++ src/FormatTime.php
@@ -2,11 +2,62 @@
 
 
 
+use Carbon\Carbon;
+
 class FormatTime
 {
-    public static function saySomething($some_string)
+    /**
+     * @param $seconds
+     * @return string
+     */
+    public static function pluralize_minute($seconds)
     {
-        $upper = strtoupper($some_string);
-        return $upper;
+        return ($seconds % (60 * 60) < 120) && ($seconds % (60 * 60) >= 60) ? "minute" : "minutes";
+    }
+
+    /**
+     * @param $seconds
+     * @return string
+     */
+    public static function pluralize_hour($seconds)
+    {
+        return $seconds < (60 * 60 * 2) ? "hour" : "hours";
+    }
+
+    /**
+     * @param $total_seconds
+     * @param bool $long_format
+     * @return string
+     */
+    public static function format_total($total_seconds, $long_format = true)
+    {
+        $hours   = floor($total_seconds / 3600);
+        $minutes = floor(($total_seconds / 60) % 60);
+        $seconds = $total_seconds % 60;
+
+        if ($long_format) {
+            $pluralize_minute = FormatTime::pluralize_minute($total_seconds);
+            $pluralize_hour   = FormatTime::pluralize_hour($total_seconds);
+
+            return ($total_format = (int)$total_seconds < (60 * 60) ? "$minutes $pluralize_minute, $seconds seconds" : "$hours $pluralize_hour, $minutes $pluralize_minute, %s seconds");
+        }
+
+        return sprintf("%02d:%02d:%02d", $hours, $minutes, $seconds);
+    }
+
+    /**
+     * @param $project_total_seconds
+     * @return mixed
+     */
+    public static function format_project_total($project_total_seconds)
+    {
+        // Sooo... converting seconds to h:i:s kinda sucks no matter which way you go
+        // especially when you need to support hour counts that exceed 24 hours
+        // I'd argue this is cleaner than chaining a bunch of floor operations.
+        $dtF = new Carbon("@0"); // What? Can't instantiate empty instance of Carbon??
+        $dtT = new Carbon("@$project_total_seconds"); // Carbon hack to make it work
+        $total_format = FormatTime::format_total($project_total_seconds);
+
+        return $project_total = $dtF->diff($dtT)->format($total_format);
     }
 }
--- src/ShowTimes.php
+++ src/ShowTimes.php
@@ -35,89 +35,17 @@ class ShowTimes extends Command
 
         $times = $this->database->selectWhere("SELECT start_time, stop_time FROM entries WHERE project_id = $project ORDER BY start_time ASC");
 
-        echo $something = FormatTime::saySomething("Drupal was hard too. \n");
-        echo $sometime  = ComputeTime::computeSomething("BUT IT GETS EASIER. \n");
+        $session = ComputeTime::compute_session_length($times);
 
+        $project_total_seconds = ComputeTime::compute_project_total_seconds($times);
 
-        /**
-         * @param $stop
-         * @param $start
-         * @return int
-         */
-        function total_in_seconds($stop, $start)
-        {
-            return (int)$elapsed_time = $stop - $start;
-        }
-
-        /**
-         * @param $total_seconds
-         * @param bool $long_format
-         * @return string
-         */
-        function format_total($total_seconds, $long_format = true)
-        {
-            $hours   = floor($total_seconds / 3600);
-            $minutes = floor(($total_seconds / 60) % 60);
-            $seconds = $total_seconds % 60;
-
-            if ($long_format) {
-                $pluralize_minute = pluralize_minute($total_seconds);
-                $pluralize_hour   = pluralize_hour($total_seconds);
-
-                return ($total_format = (int)$total_seconds < (60 * 60) ? "$minutes $pluralize_minute, $seconds seconds" : "$hours $pluralize_hour, $minutes $pluralize_minute, %s seconds");
-            }
-
-
-            return sprintf("%02d:%02d:%02d", $hours, $minutes, $seconds);
-        }
-
-        /**
-         * @param $seconds
-         * @return string
-         */
-        function pluralize_minute($seconds)
-        {
-            return ($seconds % (60 * 60) < 120) && ($seconds % (60 * 60) >= 60) ? "minute" : "minutes";
-        }
-
-        /**
-         * @param $seconds
-         * @return string
-         */
-        function pluralize_hour($seconds)
-        {
-            return $seconds < (60 * 60 * 2) ? "hour" : "hours";
-        }
-
-        $x = 0;
-        $session = [];
-        $project_total_seconds = 0;
-        foreach($times as $times_array) {
-            $total_in_seconds = total_in_seconds($times_array['stop_time'], $times_array['start_time']);
-            $total_format = format_total($total_in_seconds);
-            $session[$x]['date']  = date('M dS, Y', $times_array['start_time']);
-            $session[$x]['start'] = date('h:i A', $times_array['start_time']);
-            $session[$x]['stop']  = date('h:i A', $times_array['stop_time']);
-            $session[$x]['total'] = Carbon::createFromTimestamp($times_array['start_time'])
-                ->diff(Carbon::createFromTimestamp($times_array['stop_time']))
-                ->format($total_format);
-            $project_total_seconds+=$total_in_seconds;
-            $x++;
-        }
-
-        // Sooo... converting seconds to h:i:s kinda sucks no matter which way you go
-        // especially when you need to support times over 24 hours
-        // I'd argue this is cleaner than chaining a bunch of floor operations.
-        $dtF = new Carbon("@0"); // What? Can't instantiate empty instance of Carbon??
-        $dtT = new Carbon("@$project_total_seconds"); // Carbon hack to make it work
-        $total_format = format_total($project_total_seconds);
-        $project_total = $dtF->diff($dtT)->format($total_format);
+        $project_total_formatted = FormatTime::format_project_total($project_total_seconds);
 
         $table = new Table($output);
 
         $table->setHeaders(['Date', 'Start Time', 'Stop Time', 'Total'])->setRows($session)->render();
 
-        $output->writeln("<info>Project Totals: $project_total</info>");
+        $output->writeln("<info>Project Totals: $project_total_formatted</info>");
 
     }
 
