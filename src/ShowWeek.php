779d0a1383aaabac7925313db9096404f3fa001d
Jason Pollock <jason@monkeyhousemarketing.com>
2016-04-19 19:11:12 -0400

    Implemented pagination by week on week command

--- src/ShowWeek.php
+++ src/ShowWeek.php
@@ -4,11 +4,16 @@
 use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Helper\TableCell;
 use Symfony\Component\Console\Helper\TableSeparator;
+use Symfony\Component\Console\Input\ArgvInput;
+use Symfony\Component\Console\Input\Input;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\ConsoleOutput;
 use Symfony\Component\Console\Output\OutputInterface;
 use Carbon\Carbon;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
+use Symfony\Component\Console\Question\Question;
 
 class ShowWeek extends ShowDates {
 
@@ -27,16 +32,16 @@ class ShowWeek extends ShowDates {
          * Start of week is Monday. End of week is Sunday. That's how it is yo.
          * https://github.com/briannesbitt/Carbon/issues/175
          */
-        $week          = $input->getArgument('week');
-        $date_day      = (isset($week) ? new Carbon($week) : new Carbon());
-        $date_start    = (new Carbon($date_day))->startOfWeek()->timestamp;
-        $date_end      = (new Carbon($date_day))->endOfWeek()->timestamp;
+        $week               = $input->getArgument('week');
+        $date_week          = (isset($week) ? new Carbon($week) : new Carbon());
+        $date_week_start    = (new Carbon($date_week))->startOfWeek()->timestamp;
+        $date_week_end      = (new Carbon($date_week))->endOfWeek()->timestamp;
 
         $sessions = $this->database->selectWhere("
             SELECT id, project_id, start_time, stop_time 
             FROM entries 
             WHERE stop_time 
-            BETWEEN $date_start AND $date_end
+            BETWEEN $date_week_start AND $date_week_end
         ");
 
         $session = new Session($sessions);
@@ -45,7 +50,7 @@ class ShowWeek extends ShowDates {
 
         $project_total = $session->formatProjectTotal();
 
-        $table_header_message = "<comment>Time entries for the week of " . (new Carbon($date_day))->startOfWeek()->toFormattedDateString() . " - " . (new Carbon($date_day))->endOfWeek()->toFormattedDateString() . ": </comment>";
+        $table_header_message = "<comment>" . (new Carbon($date_week))->startOfWeek()->toFormattedDateString() . " - " . (new Carbon($date_week))->endOfWeek()->toFormattedDateString() . ": </comment>";
 
         $table_headers[] = [new TableCell($table_header_message, ['colspan' => 5])];
         $table_headers[] = ['ID', 'Date', 'Start Time', 'Stop Time', 'Session Length'];
@@ -56,5 +61,31 @@ class ShowWeek extends ShowDates {
 
         $table->setHeaders($table_headers)->setRows($session->getSessionTimes())->render();
 
+        $this->paginate($input, $output, $date_week);
+    }
+
+    public function paginate(InputInterface $input, OutputInterface $output, $starting_week) {
+        $current_week = $starting_week;
+
+        $helper = $this->getHelper('question');
+
+        $question = new Question('Display next week or previous week? ([<info>n</info>]ext|[<info>p</info>]revious) ', 'null');
+
+        $paginate = $helper->ask($input, $output, $question);
+
+        switch ($paginate) {
+            case "n":
+                $input->setArgument('week', $current_week->addWeek());
+
+                $this->execute($input, $output);
+
+                break;
+            case "p":
+                $input->setArgument('week', $current_week->subWeek());
+
+                $this->execute($input, $output);
+
+                break;
+        }
     }
 }
