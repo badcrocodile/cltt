9ff5b4903b1a2b385ab230b5d198dd358b6ee187
Jason <jpollockgc@gmail.com>
2017-04-08 15:01:15 -0500

    Added two new output methods Handles displaying archived projects in both table and list format

--- src/Command.php
+++ src/Command.php
@@ -24,11 +24,14 @@ class Command extends SymfonyCommand {
     }
 
     /**
+     * Output active projects in table format
+     *
      * @param \Symfony\Component\Console\Output\OutputInterface $output
+     *
      * @return mixed
      */
     protected function showProjectsTable(OutputInterface $output) {
-        if (!$projects = $this->database->fetchAll('projects')) {
+        if (!$projects = $this->database->fetchActiveProjects()) {
             return $output->writeln('<info>No projects at the moment!</info>');
         }
 
@@ -37,9 +40,58 @@ class Command extends SymfonyCommand {
         $table->setHeaders(['ID', 'Name'])->setRows($projects)->render();
     }
 
+    /**
+     * Output active projects in list format
+     *
+     * @param OutputInterface $output
+     *
+     * @return mixed
+     */
     protected function showProjectsList(OutputInterface $output) {
-        if (!$projects = $this->database->fetchAll('projects')) {
+        if (!$projects = $this->database->fetchActiveProjects()) {
             return $output->writeln('<error>No projects at the moment. Perhaps you\'d like to create one?</error>');
+        }
+
+        $formatter = $this->getHelper('formatter');
+
+        foreach ($projects as $id => $project) {
+            $formatted_line = $formatter->formatSection(
+                $project['id'], $project['name']
+            );
+
+            $output->writeln($formatted_line);
+        }
+    }
+
+    /**
+     * Outputs archived projects in table format
+     *
+     * @param OutputInterface $output
+     *
+     * @return mixed
+     */
+    protected function showArchivedProjectsTable(OutputInterface $output)
+    {
+        if (!$projects = $this->database->fetchArchivedProjects()) {
+            return $output->writeln('<info>No archived projects found.</info>');
+        }
+
+        $table = new Table($output);
+
+        $table->setHeaders(['ID', 'Name'])->setRows($projects)->render();
+
+    }
+
+    /**
+     * Outputs archived projects in list format
+     *
+     * @param OutputInterface $output
+     *
+     * @return mixed
+     */
+    protected function showArchivedProjectsList(OutputInterface $output) {
+        if (!$projects = $this->database->fetchArchivedProjects()) {
+            return $output->writeln('<error>No archived projects found.</error>');
         }
 
         $formatter = $this->getHelper('formatter');
